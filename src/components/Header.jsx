dbe098f8439e6c4df101409842b47e01705f0bea
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-01 11:53:35 +0900

    ✨ Create Logout function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,36 @@
 import { useContext } from "react";
-import { signInWithGoogle } from "../service/firebase";
+import { signInWithGoogle, logOut } from "../service/firebase";
+import dig from "object-dig";
 import { AuthContext } from "../providers/AuthProviders";
 
 export const Header = () => {
   const currentUser = useContext(AuthContext);
-  // console.log(currentUser)
-  return (
-    <div>
-      <button onClick={signInWithGoogle}>Login</button>
-    </div>
-  );
+  console.log(currentUser);
+  const changeLogButton = () => {
+    let changeLogButtonDom;
+    {
+      dig(currentUser, "currentUser")
+        ? (changeLogButtonDom = <button onClick={logOut}>Logout</button>)
+        : (changeLogButtonDom = (
+            <button onClick={signInWithGoogle}>Login</button>
+          ));
+    }
+    return changeLogButtonDom;
+  };
+  return <div>{changeLogButton()}</div>;
 };
+
+// export const Header = () => {
+//   const currentUser = useContext(AuthContext);
+//   console.log(currentUser)
+//   const changeLogButton = () => {
+//     let changeLogButtonDom;
+//     if (dig(currentUser, "currentUser")) {
+//       changeLogButtonDom = <button onClick={logOut}>Logout</button>;
+//     } else {
+//       changeLogButtonDom = <button onClick={signInWithGoogle}>Login</button>;
+//     }
+//     return changeLogButtonDom;
+//   };
+//   return <div>{changeLogButton()}</div>;
+// };
