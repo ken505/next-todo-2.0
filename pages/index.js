62b699ef3eb8f14b889f6e883fbab4d152beb602
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-01 14:27:53 +0900

    ✨ Import of Dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Header } from "../src/components/Header";
+import { Dashboard } from "../src/components/Dasboard";
 
 export const Home = () => {
   return (
     <div>
       <Header />
+      <Dashboard />
     </div>
   );
 }
