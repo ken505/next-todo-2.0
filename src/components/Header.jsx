b9ca9a52f22a32a2d5ca5ba5fd4077ef8c66941c
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-02 21:23:02 +0900

    💄 Add Login Logout Icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { signInWithGoogle, logOut } from "../service/firebase";
 import dig from "object-dig";
+import { LoginIcon, LogoutIcon } from "@heroicons/react/outline";
 import { AuthContext } from "../providers/AuthProviders";
 
 export const Header = () => {
@@ -10,9 +11,15 @@ export const Header = () => {
     let changeLogButtonDom;
     {
       dig(currentUser, "currentUser", "uid")
-        ? (changeLogButtonDom = <button onClick={logOut}>Logout</button>)
+        ? // ? (changeLogButtonDom = <button onClick={logOut}>Logout</button>)
+          (changeLogButtonDom = (
+            <LogoutIcon className="h-5 w-5 text-blue-500" onClick={logOut} />
+          ))
         : (changeLogButtonDom = (
-            <button onClick={signInWithGoogle}>Login</button>
+            <LoginIcon
+              className="h-5 w-5 text-blue-500"
+              onClick={signInWithGoogle}
+            />
           ));
     }
     return changeLogButtonDom;
