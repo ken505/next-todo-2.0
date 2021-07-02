431b2a4a981160f1aa61f2b5f299be520ad852ca
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-02 21:22:09 +0900

    ✨ Create initGet function

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,45 @@
 import firebase from "firebase/app";
 import { db } from "./firebase";
 
+export const initGet = async (uid) => {
+  const todo = await db
+    .collection("todo")
+    .orderBy("createdAt", "desc")
+    .where("uid", "==", uid);
+
+  return todo.get().then((snapShot) => {
+    let todos = [];
+    snapShot.forEach((doc) => {
+      // map との比較
+      // push で破壊してる
+      // console.log(doc);
+      // console.log(doc.data);
+      todos.push({
+        id: doc.id,
+        content: doc.data().content,
+        isComplete: doc.data().isComplete,
+      });
+    });
+    return todos;
+  });
+};
+
+// const [tasks, setTasks] = useState([
+//   {
+//     id: "",
+//     content: "",
+//     isComplete: "",
+//   },
+// ]);
+
+// setTasks(
+// snapshot.docs.map((doc) => ({
+//   id: doc.id,
+//   content: doc.data().content,
+//   isComplete: doc.data().isComplete,
+// }));
+// );
+
 export const addTodo = (content, uid) => {
   db.collection("todo").add({
     content: content,
@@ -10,3 +49,6 @@ export const addTodo = (content, uid) => {
   });
 };
 
+export const todoDelete = (id) => {
+  db.collection("todo").doc(id).delete();
+};
