87cf5b8d96f40d8c890e2a055a83596dc3e72c4c
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-02 21:22:28 +0900

    ✨ Create TodoList.jsx

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,36 @@
+// import { useState, useEffect, useContext } from "react";
+// import dig from "object-dig";
+// import { signInWithGoogle } from "../service/firebase";
+// import { AuthContext } from "../providers/AuthProviders";
+import { XCircleIcon } from "@heroicons/react/outline";
+import { todoDelete } from "../service/api";
+
+export const TodoList = (props) => {
+  const deleteHandler = (id) => {
+    todoDelete(id);
+    // ↓ 講座の動画上では props の表記、説明は無いので記述漏れに注意
+    // あと、動作する時としない時があるのが謎い
+    props.initialDataFetch();
+  };
+  const todoList = props.todos.map((todo) => {
+    return (
+      <li className="flex" key={todo.id}>
+        <input type="checkbox" className=" h-4 w-4 text-blue-500" />
+        {todo.content}
+        <XCircleIcon
+          className="h-5 w-5 text-blue-500"
+          onClick={() => deleteHandler(todo.id)}
+        />
+        {/* <button
+          // ↓ いらなくね？
+          // form ならいるかもしれんけど
+          // type="button"
+          onClick={() => deleteHandler(todo.id)}
+        >
+          🗑
+        </button> */}
+      </li>
+    );
+  });
+  return <ul>{todoList}</ul>;
+};
