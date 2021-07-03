056d1a7b32d2dbe8babece584767eb67dfa10753
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-03 19:19:34 +0900

    💄 Style index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,14 @@ import { Dashboard } from "../src/components/Dashboard";
 
 export const Home = () => {
   return (
-    <div>
+    <div
+      className="min-h-screen
+      px-5 py-16 
+      font-mono text-gray-100 dark:text-gray-400
+      bg-gradient-to-tr 
+    from-purple-400 via-blue-300 to-green-100
+    dark:from-gray-900 dark:via-pink-500 dark:to-purple-800"
+    >
       <Header />
       <Dashboard />
     </div>
