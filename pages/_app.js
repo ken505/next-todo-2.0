05c659dde696fd0eafcef07b494460277be06c22
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-03 15:06:34 +0900

    ✨ Add nextpwa function

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../src/service/firebase";
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
 import { AuthProvider } from "../src/providers/AuthProviders";
 
 const MyApp = (props) => {
   return (
     <AuthProvider>
+      <Head>
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <props.Component {...props.pageProps} />
     </AuthProvider>
   );
