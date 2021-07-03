c14ea3ba6ffa3f7af73d31f9926206315f601c11
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-03 17:14:09 +0900

    ✨ Add Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,39 @@
+import Head from "next/head";
 import "../src/service/firebase";
 import "tailwindcss/tailwind.css";
 import { AuthProvider } from "../src/providers/AuthProviders";
 
-const MyApp = (props) => {
+export default function MyApp(props) {
   return (
     <AuthProvider>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="Todo-App" content="Todo-App" />
+        <meta name="Todo-App" content="Todo-App" />
+        <title>Next.js PWA Todo-App</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <props.Component {...props.pageProps} />
     </AuthProvider>
   );
-};
-
-export default MyApp;
+}
