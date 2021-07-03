b44a5ce65e71da4f1d8871aea6773c1bb97ead18
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-03 19:25:43 +0900

    💄

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
       px-5 py-16 
       font-mono text-gray-100 dark:text-gray-400
       bg-gradient-to-tr 
-    from-purple-400 via-blue-300 to-green-100
+    from-pink-400 via-blue-300 to-green-200
     dark:from-gray-900 dark:via-pink-500 dark:to-purple-800"
     >
       <Header />
