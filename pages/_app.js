ed24b985a07992a66424556d4ee5b622a8b14358
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-03 15:20:52 +0900

    🐛 _app code recovery

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,6 @@ import { AuthProvider } from "../src/providers/AuthProviders";
 const MyApp = (props) => {
   return (
     <AuthProvider>
-      <Head>
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="manifest" href="/manifest.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-      </Head>
       <props.Component {...props.pageProps} />
     </AuthProvider>
   );
