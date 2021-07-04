a8edb7e0f0f62b9b4817d07af43576763a8058ac
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-04 14:52:06 +0900

    💬 update text

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -49,9 +49,9 @@ export const Dashboard = () => {
               >
                 Add
               </button> */}
+
               <PlusCircleIcon
-                className="h-10 w-10 ml-3 cursor-pointer hover:opacity-60 dark:hover:opacity-5
-                flex items-center self-center"
+                className="h-10 w-10 ml-3 cursor-pointer hover:opacity-60 dark:hover:opacity-5"
                 disabled={inputName.length > 0 ? false : true}
                 type="button"
                 onClick={() => post()}
